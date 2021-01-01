d09c8f7c758cd38acf792df8332163ced9b52d79
Atakan Yasin Uludağ <atknuludag@gmail.com>
2021-01-01 21:22:10 +0300

    Notfound sayfası eklendi.

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Form, FormControl, Button, Row, Col } from 'react-bootstrap';
 import FullLayout from '../components/FullLayout';
 
 type NotFoundComponent = React.FC & { layout: typeof FullLayout }
@@ -6,8 +7,24 @@ type NotFoundComponent = React.FC & { layout: typeof FullLayout }
 const NotFound: NotFoundComponent = () => {
 
   return (
-    <div>
-      <h1>TEST</h1>
+    <div className="box box-padding">
+
+      <div className="not-found">
+        <h1>Sayfa bulunamadı</h1>
+        <p>Aradığınız sayfa bulunamadı. İsterseniz tekrar arama yapabilirsiniz.</p>
+        <Form>
+          <Row>
+            <Col md={9}>
+              <FormControl type="text" size="lg" placeholder="Sitede ara..." className="mb-2" />
+            </Col>
+            <Col md={3}>
+              <Button variant="primary" size="lg"><i className="fas fa-search"></i></Button>
+            </Col>
+          </Row>
+        </Form>
+      </div>
+      
+
     </div>
   )
 }
